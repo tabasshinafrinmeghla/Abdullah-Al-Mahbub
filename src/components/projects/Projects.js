6e6b094ae831cb9c329f26869397f5961979883e
tabasshinafrinmeghla <tabasshinafrinmeghla@gmail.com>
2024-03-03 18:04:08 +0600

    added all

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Title from '../layouts/Title'
 import { projectOne, projectTwo, projectThree } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
+import { FaFacebook } from 'react-icons/fa';
 
 const Projects = () => {
+
+  
   return (
     <section
       id="projects"
@@ -27,17 +30,25 @@ const Projects = () => {
           des=" Web Connection, founded in 2019, is a prominent Internet Service Provider (ISP) company committed to delivering cutting-edge connectivity solutions. With a solid foundation in technology and a customer-first approach, Web Connection has rapidly become a trusted name in the industry, providing reliable internet services to meet the evolving needs of individuals and businesses alike."
           src={projectTwo}
         />
+        <div>
         <ProjectsCard
           title="Food Bank Cafe Bhairab
           "
           des="Food Bank Cafe, established in 2019, is not just a restaurant but a culinary haven where flavors meet compassion. Rooted in the mission to alleviate hunger, this unique establishment serves up delicious meals while also giving back to the community. With every bite, patrons support a noble cause, making dining at Food Bank Cafe an experience that nourishes both the body and soul."
           src={projectThree}
         />
+        <div>
+          <a href='https://www.facebook.com/'><FaFacebook></FaFacebook></a>
+        </div>
+        </div>
         {/* <ProjectsCard
+         
           title="IT Solution"
+        
           des=
           "IT Solution, poised to launch in the coming year, is a forward-thinking security-based company dedicated to safeguarding digital assets and protecting businesses from cyber threats. With a focus on cutting-edge technology and innovative strategies, IT Solution is poised to redefine cybersecurity standards, offering comprehensive solutions to address the evolving landscape of digital security challenges."
           src={projectTwo}
+
         /> */}
       </div>
     </section>
