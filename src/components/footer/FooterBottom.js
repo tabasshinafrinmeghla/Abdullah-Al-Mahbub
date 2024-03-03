a0f2db3c8dc912b2d12e6ca0a21f3373d6c7e2dd
tabasshinafrinmeghla <tabasshinafrinmeghla@gmail.com>
2024-03-03 12:00:55 +0600

    added

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { FaFontAwesome, FaPhone } from 'react-icons/fa';
+
+// import {  } from 'react-icons/fa';
 
 const FooterBottom = () => {
   return (
-    <div className="w-full py-10">
+    <div className="w-full py-10 ">
       <p className="text-center text-gray-500 text-base">
-        © 2022. All rights reserved by Noor Mohammad
+        © 2024. All rights reserved by Abdullah Al Mahbub
+      </p>
+      <br/>
+      <p className="text-center text-gray-500 text-base">
+        DEVELOPERS @tabasshin Afrin Meghla 
+        <FaFontAwesome icon={FaPhone} />
+        +8801745771186
       </p>
     </div>
   );
